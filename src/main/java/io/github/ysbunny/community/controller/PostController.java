b28c92c5e1d368c39e8847b7707837b480afe4db
selina.yang <yun0836@naver.com>
2026-06-07 18:37:59 +0900

    feat: PostController 생성

---
+++ src/main/java/io/github/ysbunny/community/controller/PostController.java
@@ -0,0 +1,37 @@
+package io.github.ysbunny.community.controller;
+
+import io.github.ysbunny.community.dto.post.*;
+import io.github.ysbunny.community.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/posts")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public CreatePostResponse createPost(@RequestBody CreatePostRequest request) {
+        return postService.createPost(request);
+    }
+
+    @GetMapping("/{postId}")
+    public PostDetailResponse getPost(@PathVariable Long postId) {
+        return postService.getPost(postId);
+    }
+
+    @PutMapping("/{postId}")
+    public UpdatePostResponse updatePost(
+            @PathVariable Long postId,
+            @RequestBody UpdatePostRequest request
+    ) {
+        return postService.updatePost(postId, request);
+    }
+
+    @DeleteMapping("/{postId}")
+    public DeletePostResponse deletePost(@PathVariable Long postId) {
+        return postService.deletePost(postId);
+    }
+}

29c3e9fd3d0ba12bc10de620e4b63d6b8e810639
selina.yang <yun0836@naver.com>
2026-06-07 18:55:20 +0900

    feat: CreateCommentRequest, CreateCommentResponse DTO 생성

---
+++ src/main/java/io/github/ysbunny/community/dto/comment/CreateCommentRequest.java
@@ -0,0 +1,8 @@
+package io.github.ysbunny.community.dto.comment;
+
+import lombok.Getter;
+
+@Getter
+public class CreateCommentRequest {
+    private String comment;
+}
---
+++ src/main/java/io/github/ysbunny/community/dto/comment/CreateCommentResponse.java
@@ -0,0 +1,10 @@
+package io.github.ysbunny.community.dto.comment;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateCommentResponse {
+    private Long commentId;
+}

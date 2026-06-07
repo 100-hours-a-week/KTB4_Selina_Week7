6b0301ce7cbb497074d4af4748936244b16dcb60
selina.yang <yun0836@naver.com>
2026-06-07 17:09:25 +0900

    feat: UserController 생성

---
+++ src/main/java/io/github/ysbunny/community/controller/UserController.java
@@ -0,0 +1,42 @@
+package io.github.ysbunny.community.controller;
+
+import io.github.ysbunny.community.dto.user.*;
+import io.github.ysbunny.community.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/sign-up")
+    public CreateUserResponse createUser(@RequestBody CreateUserRequest request) {
+        return userService.createUser(request);
+    }
+
+    @PostMapping("/log-in")
+    public LoginUserResponse logIn(@RequestBody LoginUserRequest request) {
+        return userService.login(request);
+    }
+
+    @PatchMapping("/{userId}")
+    public UpdateUserResponse updateUser(
+            @PathVariable Long userId,
+            @RequestBody UpdateUserRequest request
+    ) {
+        return userService.updateUser(userId, request);
+    }
+
+    @PostMapping("/log-out")
+    public LogoutUserResponse logout(@RequestBody LogoutUserRequest request) {
+        return userService.logout(request);
+    }
+
+    @DeleteMapping("/{userId}")
+    public DeleteUserResponse deleteUser(@PathVariable Long userId) {
+        return userService.deleteUser(userId);
+    }
+}

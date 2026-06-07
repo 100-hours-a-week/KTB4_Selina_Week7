dada31fb37803e1d2e34ae2352635abdaa0bf54e
selina.yang <yun0836@naver.com>
2026-06-07 16:34:19 +0900

    feat: UpdateUserResponse, DeleteUserResponse에 @AllArgsConstructor 추가

--- src/main/java/io/github/ysbunny/community/dto/user/DeleteUserResponse.java
+++ src/main/java/io/github/ysbunny/community/dto/user/DeleteUserResponse.java
@@ -1,8 +1,10 @@
 package io.github.ysbunny.community.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class DeleteUserResponse {
     private String message;
 }

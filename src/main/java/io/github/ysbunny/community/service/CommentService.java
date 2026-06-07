c7aad45d7085396369e682863142ab3946f7fbb8
selina.yang <yun0836@naver.com>
2026-06-07 19:01:21 +0900

    feat: CommentService 생성

---
+++ src/main/java/io/github/ysbunny/community/service/CommentService.java
@@ -0,0 +1,22 @@
+package io.github.ysbunny.community.service;
+
+import io.github.ysbunny.community.domain.Comment;
+import io.github.ysbunny.community.dto.comment.CreateCommentRequest;
+import io.github.ysbunny.community.dto.comment.CreateCommentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private Comment comment;
+
+    public CreateCommentResponse createComment(CreateCommentRequest request) {
+        comment = new Comment(
+                1L,
+                request.getComment()
+        );
+        return new CreateCommentResponse(comment.getCommentId());
+    }
+}

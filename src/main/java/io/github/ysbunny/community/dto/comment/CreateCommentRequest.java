bf38345b6df14e1128c831e6465315d4ae58c0dd
selina.yang <yun0836@naver.com>
2026-06-07 23:51:48 +0900

    feat: 용도 별 requestDto에 Bean Validation 제약 조건 추가

--- src/main/java/io/github/ysbunny/community/dto/comment/CreateCommentRequest.java
+++ src/main/java/io/github/ysbunny/community/dto/comment/CreateCommentRequest.java
@@ -1,8 +1,11 @@
 package io.github.ysbunny.community.dto.comment;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class CreateCommentRequest {
+
+    @NotBlank
     private String comment;
 }
--- src/main/java/io/github/ysbunny/community/dto/user/LogoutUserRequest.java
+++ src/main/java/io/github/ysbunny/community/dto/user/LogoutUserRequest.java
@@ -1,8 +1,11 @@
 package io.github.ysbunny.community.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class LogoutUserRequest {
+
+    @NotBlank
     private String token;
 }

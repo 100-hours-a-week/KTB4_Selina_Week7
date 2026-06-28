f65b0856a4b240e76e3426b6056833f495a83d70
seul_bunny <yun0836@naver.com>
2026-06-28 11:54:06 +0900

    feat: 회원정보 기능 수정

--- client/js/profile-edit.js
+++ client/js/profile-edit.js
@@ -1,5 +1,40 @@
 // 0. HTML이 다 로드된 뒤 이벤트 리스너를 등록
 document.addEventListener("DOMContentLoaded", function () {
+    // 1. 프로필 수정 폼 가져옴
+    const profileEditForm = document.querySelector("#profileEditForm");
+
+    profileEditForm.addEventListener("submit", function (event) {
+        // 2. 회원정보 수정 폼 제출 기본 동작 막음
+        event.preventDefault();
+    
+        // 1. 프로필 프리뷰, 프로필 이미지, 닉네임 가져옴
+        const profilePreview = document.querySelector("#profilePreview");
+        const profileImage = document.querySelector("#profileImage");
+        const nicknameInput = document.querySelector("#nickname");
+
+        const nickname = nicknameInput.value.trim();
+
+        // 2. 프로필 이미지 값이 바뀌면 프로필 프리뷰도 바뀜 구현
+
+
+        // 2. 닉네임 값 입력할 때마다 검사
+        nicknameInput.addEventListener("input", () => {
+            
+        });
+
+        // 3. 닉네임 입력됐으면 회원정보 수정 가능
+        if (!(nickname === "")) {
+            // 4. 수정완료 토스트 메시지 2초간 띄움
+            const editCompleteToast = document.querySelector("#editCompleteToast");
+
+            editCompleteToast.classList.remove("is-hidden");
+
+            setTimeout(() => {
+                editCompleteToast.classList.add("is-hidden");
+            }, 2000);
+        }
+    });
+
     // 1. 회원 탈퇴 모달창 가져옴
     const withdrawButton = document.querySelector("#withdrawButton");
 

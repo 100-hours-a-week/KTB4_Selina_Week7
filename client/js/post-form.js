04dd8579e47cbb3e9b2a58d3a50365103ccf0eee
seul_bunny <yun0836@naver.com>
2026-06-28 16:42:43 +0900

    feat: 게시글 작성 페이지 제목, 내용 작성 유도 안내 문구 구현

--- client/js/post-form.js
+++ client/js/post-form.js
@@ -18,4 +18,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // 해당 게시글 데이터 불러오는 로직 나중에 추가
     }
+
+    // 1. 게시글 폼, 제목, 내용, 안내 문구 가져옴
+    const postForm = document.querySelector("#postForm");
+    const titleInput = document.querySelector("#title");
+    const contentInput = document.querySelector("#content");
+    const helperText = document.querySelector("#helperText");
+
+    postForm.addEventListener("submit", (event) => {
+        // 2. 게시글 작성 폼 제출 기본 동작 막음
+        event.preventDefault();
+
+        const title = titleInput.value.trim();
+        const content = contentInput.value.trim();
+
+        // 3. 안내 문구가 처음에는 없고 제목이나 내용 값이 비면 생김
+        helperText.textContent = "";
+
+        if (title === "" || content === "") {
+            helperText.textContent = "* 제목, 내용을 모두 작성해주세요.";
+
+            // 4. 게시글 작성 막음
+            return;
+        }
+
+        console.log("게시글 작성 가능");
+        console.log("제목:", title);
+        console.log("내용:", content);
+        
+        // 해당 게시글 데이터 업데이트 로직 나중에 추가
+    });
 });

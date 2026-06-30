cef40a1e9a88c376ffc6469fbc439e4231a24a1d
seul_bunny <yun0836@naver.com>
2026-06-30 19:29:46 +0900

    feat: 로그인 페이지 안내 문구 기능, 로그인 버튼 활성화 기능 구현

--- client/js/login.js
+++ client/js/login.js
@@ -1,50 +1,67 @@
 // 0. HTML이 다 로드된 뒤 이벤트 리스너를 등록
 document.addEventListener("DOMContentLoaded", function () {
-    // 1. 로그인 폼 가져옴
+    // 1. 로그인 폼, 이메일 폼, 비밀번호 폼, 안내 문구들, 로그인 버튼 가져옴
     const loginForm = document.querySelector("#loginForm");
 
-    loginForm.addEventListener("submit", function (event) {
+    const emailInput = document.querySelector("#email");
+    const passwordInput = document.querySelector("#password");
+
+    const emailHelperText = document.querySelector("#emailHelperText");
+    const passwordHelperText = document.querySelector("#passwordHelperText");
+
+    const loginButton = document.querySelector("#loginButton");
+
+    loginForm.addEventListener("submit", (event) => {
         // 2. 로그인 폼 제출 기본 동작 막음
         event.preventDefault();
 
-        // 3. 이메일 값, 비밀번호 값, 안내 문구들 가져오기
-        const emailInput = document.querySelector("#email");
-        const passwordInput = document.querySelector("#password");
+        // 3. 이메일 값, 비밀번호 값 가져옴
+        const email = emailInput.value.trim();
+        const password = passwordInput.value.trim();
 
+        // 4. 이메일과 비밀번호 모두 값이 있을 때 로그인되어 게시글 목록으로 이동
+        if (email !== "" && password !== "") {
+            window.location.href = `./posts.html`;
+        }
+    });
+
+    emailInput.addEventListener("input", () => {
+        // 2. 이메일 값, 비밀번호 값 가져옴
         const email = emailInput.value.trim();
         const password = passwordInput.value.trim();
         
-        const emailHelperText = document.querySelector("#emailHelperText");
-        const passwordHelperText = document.querySelector("#passwordHelperText");
+        // 3. 이메일 값 여부에 따라 안내 문구 여부 변경
+        if (email === "") {
+            emailHelperText.textContent = "* 이메일을 입력해주세요.";
+        } else {
+            emailHelperText.textContent = "";
+        }
 
-        // 4. 이메일 값 입력할 때마다 검사
-        emailInput.addEventListener("input", () => {
-            // 5. 값이 없으면 emailHelperText 안내 문구 띄움
-            // if (email === "") {
-            //     emailHelperText.textContent = "* 이메일을 입력해주세요.";
-            // } else {
-            //     emailHelperText.textContent = "";
-            // }
+        // 4. 이메일과 비밀번호 값 둘 다 있으면 로그인 버튼 활성화
+        if (email !== "" && password !== "") {
+            loginButton.disabled = false;
+        } else {
+            loginButton.disabled = true;
+        }
+    });
 
-            // 이메일 검증 로직
-        });
+    passwordInput.addEventListener("input", () => {
+        // 2. 이메일 값, 비밀번호 값 가져옴
+        const email = emailInput.value.trim();
+        const password = passwordInput.value.trim();
 
-        // 4. 비밀번호 값 입력할 때마다 검사
-        passwordInput.addEventListener("input", () => {
-            // 5. 값이 없으면 passwordHelperText 안내 문구 띄움
-            // if (password === "") {
-            //     passwordHelperText.textContent = "* 비밀번호를 입력해주세요.";
-            // } else {
-            //     passwordHelperText.textContent = "";
-            // }
+        // 3. 비밀번호 값 여부에 따라 안내 문구 여부 변경
+        if (password === "") {
+            passwordHelperText.textContent = "* 비밀번호를 입력해주세요.";
+        } else {
+            passwordHelperText.textContent = "";
+        }
 
-            // 비밀번호 검증 로직
-        });
-
-        // 6. 이메일, 비밀번호 둘 다 입력됐으면 로그인 가능
-        if (!(email === "" || password === "")) {
-            // 7. 게시글 목록 페이지로 이동
-            window.location.href = `./posts.html`;
+        // 4. 이메일과 비밀번호 값 둘 다 있으면 로그인 버튼 활성화
+        if (email !== "" && password !== "") {
+            loginButton.disabled = false;
+        } else {
+            loginButton.disabled = true;
         }
     });
 

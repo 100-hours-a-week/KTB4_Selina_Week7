ead230dfbb193750f9fd1b143b6ae2443c1c2cf8
seul_bunny <yun0836@naver.com>
2026-07-02 20:40:40 +0900

    feat: 로그인 페이지 백엔드와 연결

--- client/js/login.js
+++ client/js/login.js
@@ -1,75 +1,87 @@
-// 0. HTML이 다 로드된 뒤 이벤트 리스너를 등록
-document.addEventListener("DOMContentLoaded", function () {
-    // 1. 로그인 폼, 이메일 폼, 비밀번호 폼, 안내 문구들, 로그인 버튼 가져옴
-    const loginForm = document.querySelector("#loginForm");
+import { login } from "./api/userApi.js";
+ 
+const loginForm = document.querySelector("#loginForm");
 
-    const emailInput = document.querySelector("#email");
-    const passwordInput = document.querySelector("#password");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
 
-    const emailHelperText = document.querySelector("#emailHelperText");
-    const passwordHelperText = document.querySelector("#passwordHelperText");
+const emailHelperText = document.querySelector("#emailHelperText");
+const passwordHelperText = document.querySelector("#passwordHelperText");
 
-    const loginButton = document.querySelector("#loginButton");
+const loginButton = document.querySelector("#loginButton");
 
-    loginForm.addEventListener("submit", (event) => {
-        // 2. 로그인 폼 제출 기본 동작 막음
-        event.preventDefault();
+loginForm.addEventListener("submit", async (event) => {
+    // 2. 로그인 폼 제출 기본 동작 막음
+    event.preventDefault();
 
-        // 3. 이메일 값, 비밀번호 값 가져옴
-        const email = emailInput.value.trim();
-        const password = passwordInput.value.trim();
+    // 이메일 값, 비밀번호 값 가져와서 userData 객체 생성
+    const userData = {
+        email: emailInput.value.trim(),
+        password: passwordInput.value.trim()
+    }
 
-        // 4. 이메일과 비밀번호 모두 값이 있을 때 로그인되어 게시글 목록으로 이동
-        if (email !== "" && password !== "") {
+    console.log("로그인 요청 데이터:", userData);
+
+    // 4. 이메일과 비밀번호 모두 값이 있을 때 로그인되어 게시글 목록으로 이동
+    if (userData.email !== "" && userData.password !== "") {
+        try {
+            // userData 객체를 백엔드에 보내고 받은 JSON userId와 토큰을 result에 저장
+            const result = await login(userData);
+
+            console.log(`userId: ${result.userId}`);
+            console.log(`token: ${result.token}`);
+
             window.location.href = `./posts.html`;
+        } catch (error) {
+            alert(error.message);
         }
-    });
+    }
+});
 
-    emailInput.addEventListener("input", () => {
-        // 2. 이메일 값, 비밀번호 값 가져옴
-        const email = emailInput.value.trim();
-        const password = passwordInput.value.trim();
-        
-        // 3. 이메일 값 여부에 따라 안내 문구 여부 변경
-        if (email === "") {
-            emailHelperText.textContent = "* 이메일을 입력해주세요.";
-        } else {
-            emailHelperText.textContent = "";
-        }
+emailInput.addEventListener("input", () => {
+    // 2. 이메일 값, 비밀번호 값 가져옴
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+    
+    // 3. 이메일 값 여부에 따라 안내 문구 여부 변경
+    if (email === "") {
+        emailHelperText.textContent = "* 이메일을 입력해주세요.";
+    } else {
+        emailHelperText.textContent = "";
+    }
 
-        // 4. 이메일과 비밀번호 값 둘 다 있으면 로그인 버튼 활성화
-        if (email !== "" && password !== "") {
-            loginButton.disabled = false;
-        } else {
-            loginButton.disabled = true;
-        }
-    });
+    // 4. 이메일과 비밀번호 값 둘 다 있으면 로그인 버튼 활성화
+    if (email !== "" && password !== "") {
+        loginButton.disabled = false;
+    } else {
+        loginButton.disabled = true;
+    }
+});
 
-    passwordInput.addEventListener("input", () => {
-        // 2. 이메일 값, 비밀번호 값 가져옴
-        const email = emailInput.value.trim();
-        const password = passwordInput.value.trim();
+passwordInput.addEventListener("input", () => {
+    // 2. 이메일 값, 비밀번호 값 가져옴
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
 
-        // 3. 비밀번호 값 여부에 따라 안내 문구 여부 변경
-        if (password === "") {
-            passwordHelperText.textContent = "* 비밀번호를 입력해주세요.";
-        } else {
-            passwordHelperText.textContent = "";
-        }
+    // 3. 비밀번호 값 여부에 따라 안내 문구 여부 변경
+    if (password === "") {
+        passwordHelperText.textContent = "* 비밀번호를 입력해주세요.";
+    } else {
+        passwordHelperText.textContent = "";
+    }
 
-        // 4. 이메일과 비밀번호 값 둘 다 있으면 로그인 버튼 활성화
-        if (email !== "" && password !== "") {
-            loginButton.disabled = false;
-        } else {
-            loginButton.disabled = true;
-        }
-    });
+    // 4. 이메일과 비밀번호 값 둘 다 있으면 로그인 버튼 활성화
+    if (email !== "" && password !== "") {
+        loginButton.disabled = false;
+    } else {
+        loginButton.disabled = true;
+    }
+});
 
-    // 1. 회원가입 버튼 가져옴
-    const signupButton = document.querySelector("#signupButton");
+// 1. 회원가입 버튼 가져옴
+const signupButton = document.querySelector("#signupButton");
 
-    signupButton.addEventListener("click", function () {
-        // 2. 회원가입 페이지로 이동
-        window.location.href = `./signup.html`;
-    });
+signupButton.addEventListener("click", function () {
+    // 2. 회원가입 페이지로 이동
+    window.location.href = `./signup.html`;
 });
